5f3506dd37fb917fe5f98a29e0cf28eb65e7a0e3
virajbodake <virajbodake7172@gmail.com>
2025-05-14 14:02:28 +0530

    14/06/2025

---
+++ src/ex_01_java_basics/lab04_Comment.java
@@ -0,0 +1,10 @@
+package ex_01_java_basics;
+
+public class lab04_Comment {
+    public static void main(String[] args) {
+
+        byte age = 25; // age is asssigned 25 value( single line comment)
+        System.out.println(age);
+    }
+
+}
